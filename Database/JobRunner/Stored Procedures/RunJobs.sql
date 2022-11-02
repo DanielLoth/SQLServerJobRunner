450bd49e0309bdf77d4da6b5736487409e4f0feb
Daniel Loth <lothyis@gmail.com>
2022-11-02 23:12:03 +1100

    WIP

--- Database/JobRunner/Stored Procedures/RunJobs.sql
+++ Database/JobRunner/Stored Procedures/RunJobs.sql
@@ -1,5 +1,29 @@
 ﻿create procedure JobRunner.RunJobs
 as
-select 1 as A;
+
+set nocount, xact_abort on;
+set deadlock_priority low;
+
+declare
+	@LockTimeoutSeconds int = -1, /* Default lock timeout, infinite wait */
+	@DeadlockPriority int = 0, /* Default deadlock priority */
+	@StartDtmUtc datetime2(0) = getutcdate();
+
+if @LockTimeout = -1 set lock_timeout -1; /* Wait forever */
+else if @LockTimeout = 0 set lock_timeout 0; /* Don't wait at all */
+else if @LockTimeout = 1 set lock_timeout 1000;
+else if @LockTimeout = 2 set lock_timeout 2000;
+else if @LockTimeout = 3 set lock_timeout 3000;
+else if @LockTimeout = 4 set lock_timeout 4000;
+else if @LockTimeout = 5 set lock_timeout 5000;
+else if @LockTimeout = 6 set lock_timeout 6000;
+else if @LockTimeout = 7 set lock_timeout 7000;
+else if @LockTimeout = 8 set lock_timeout 8000;
+else if @LockTimeout = 9 set lock_timeout 9000;
+else if @LockTimeout = 10 set lock_timeout 10000;
+else if @LockTimeout = 15 set lock_timeout 15000;
+else if @LockTimeout = 30 set lock_timeout 30000;
+else if @LockTimeout = 60 set lock_timeout 60000;
+else if @LockTimeout = 120 set lock_timeout 120000;
 
 go
---
+++ Database/JobRunner/Tables/Config.sql
@@ -0,0 +1,21 @@
+﻿create table JobRunner.Config (
+	RowNumber int not null constraint JobRunner_Config_RowNumber_DF default 1,
+	MaxJobRunnerExecTimeSeconds int not null,
+	NumRowsPerBatch int not null,
+	DeadlockPriority int not null,
+	LockTimeoutSeconds int not null,
+	MaxSyncSecondaryCommitLatencySeconds int not null,
+	MaxRedoQueueSize int not null,
+	MaxProcedureExecTimeViolationCount int not null,
+	MaxProcedureExecTimeMilliseconds int not null,
+
+	constraint JobRunner_Config_Has_SingleRow_CK check (RowNumber = 1),
+
+	constraint JobRunner_Config_Has_Valid_DeadlockPriority_CK
+	check (DeadlockPriority between -10 and 10),
+
+	constraint JobRunner_Config_Has_Valid_LockTimeoutSeconds_CK
+	check (LockTimeoutSeconds between -1 and 10 or LockTimeoutSeconds in (15, 30, 60, 120))
+);
+
+go
