7d458e2c948a5268158ca0225d53186d927a86ff
Daniel Loth <lothyis@gmail.com>
2022-11-02 20:49:36 +1100

    Add initial structure to the solution

---
+++ Database/dbo/Tables/Person.sql
@@ -0,0 +1,8 @@
+﻿create table dbo.Person (
+	PersonNumber bigint not null,
+	GivenNames nvarchar(100) not null,
+	FamilyName nvarchar(100) not null,
+
+	constraint UC_Person_PK primary key clustered (PersonNumber)
+);
+go
