ec19e6f49fab151c95b54bd39b401b33f3f2f06b
Daniel Loth <daniel.loth@xero.com>
2022-11-05 16:54:11 +1100

    Add project with demo data and objects

---
+++ JobRunnerWithDemoData/dbo/Stored Procedures/UpdateGuidValJob.sql
@@ -0,0 +1,14 @@
+﻿create procedure dbo.UpdateGuidValJob
+as
+
+set nocount, xact_abort on;
+
+with v as (
+	select top 5 *
+	from dbo.GuidVal
+	order by LastUpdatedDtmUtc
+)
+update v
+set Val = newid(), LastUpdatedDtmUtc = getutcdate();
+
+return 0;
---
+++ JobRunnerWithDemoData/dbo/Tables/GuidVal.sql
@@ -0,0 +1,6 @@
+﻿create table dbo.GuidVal (
+	Val uniqueidentifier not null,
+	LastUpdatedDtmUtc datetime2(0),
+
+	constraint UC_GuidVal_PK primary key (Val)
+);
