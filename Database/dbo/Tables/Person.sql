4581e7bf32444107de4f78d8ad3705a8f8271540
Daniel Loth <daniel.loth@xero.com>
2022-11-05 16:28:34 +1100

    Remove Person table (unused)

--- Database/dbo/Tables/Person.sql
+++
@@ -1,8 +0,0 @@
-﻿create table dbo.Person (
-	PersonNumber bigint not null,
-	GivenNames nvarchar(100) not null,
-	FamilyName nvarchar(100) not null,
-
-	constraint UC_Person_PK primary key clustered (PersonNumber)
-);
-go
