bd3702c5f47d950806f414163f36d12d7668df62
Daniel Loth <daniel.loth@xero.com>
2022-12-09 10:00:27 +1100

    WIP

--- .batect/sql-shared/01-restore-backup.sh
+++ .batect/sql-shared/01-restore-backup.sh
@@ -3,5 +3,5 @@ set -e
 
 export PATH="$PATH:/opt/mssql-tools/bin/"
 
-sqlcmd -U sa -S "localhost,1433" -P $MSSQL_SA_PASSWORD -i /sql-shared/sql-scripts/01-restore-backup.sql
+sqlcmd -U sa -S "$NODE1_HOSTNAME,1433" -P $MSSQL_SA_PASSWORD -i /sql-shared/sql-scripts/01-restore-backup.sql
 
