311c3c91749afe299af1080e031c200f956dd3de
Daniel Loth <lothyis@gmail.com>
2022-12-12 22:41:00 +1100

    Remove no longer required bash scripts

--- .batect/sql-shared/delete-cert-files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -o pipefail
-
-# Delete cert files if they exist. They'll be created again as part of the HADR setup.
-rm -f /certificate/cert.key 2> /dev/null
-rm -f /certificate/cert.cert 2> /dev/null
